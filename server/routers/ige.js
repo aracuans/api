6ba3c0219f78dd7a02486fc4ca670e969fe20dba
aracuans <75974459+aracuans@users.noreply.github.com>
2021-01-14 08:40:29 +0700

    Update ige.js

--- server/routers/ige.js
+++ server/routers/ige.js
@@ -12,11 +12,10 @@ async function getIge(URL) {
     await page.type('#downloaderform-url', `${URL}`);
     await page.click('#search', { delay: 300 });
 
-    await page.waitForSelector('#ajax-results > div > div:nth-child(1) > div > div > a', {delay: 300});
-    let LinkDownload = await page.$eval("#ajax-results > div > div:nth-child(1) > div > div > a", (element) => {
+    await page.waitForSelector('#ajax-results > div > div:nth-child(1) > div > div > a');
+    let LinkDownload = await page.$eval('#ajax-results > div > div:nth-child(1) > div > div > a', (element) => {
         return element.getAttribute("href");
     });
-	let creator = ('CUANS');
 	let username = await page.$eval('#ajax-results > div > div:nth-child(2) > div > div > div.da-username.da-icon-text > span', el => el.innerText);
 	browser.close();
     return { LinkDownload, username, creator }
