d82968ddda42d82e0627cd28e9a55e2d60402800
aracuans <75974459+aracuans@users.noreply.github.com>
2021-01-14 22:44:50 +0700

    Update ytmus.js

--- server/routers/ytmus.js
+++ server/routers/ytmus.js
@@ -21,10 +21,10 @@ async function ytMusic(URL) {
     let LinkAudio = await page.$eval('#download > div > div > a', (element) => {
         return element.getAttribute('href');
     });
- /* let titleInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(1)', el => el.innerText);
+    let titleInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(1)', el => el.innerText);
     let sizeInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(2)', el => el.innerText); */
 	browser.close();
-    return { thumb, LinkAudio, }
+    return { thumb, LinkAudio, titleInfo, sizeInfo }
 }
 
 ytmus.get('/', async (req, res) => {
