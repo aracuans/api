3af3c29f38012a24912567f2d56ed3d648741ea6
aracuans <75974459+aracuans@users.noreply.github.com>
2021-01-14 08:47:34 +0700

    Update ytmus.js

--- server/routers/ytmus.js
+++ server/routers/ytmus.js
@@ -12,19 +12,19 @@ async function ytMusic(URL) {
     await page.type('#inputUrl', `${URL}`);
     await page.click('body > div:nth-child(1) > div:nth-child(2) > form > p:nth-child(5) > button', {delay: 300});
     await page.waitForSelector('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-3 > img');
-    let getImages = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-3 > img', (element) => {
+    let thumb = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-3 > img', (element) => {
 	return element.getAttribute('src');
     });
     await page.waitForSelector('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > div.container > div > p > button');
     await page.click('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > div.container > div > p > button');
     await page.waitForSelector('#download > div > div > a');
-    let getVideo = await page.$eval('#download > div > div > a', (element) => {
+    let LinkAudio = await page.$eval('#download > div > div > a', (element) => {
         return element.getAttribute('href');
     });
  /* let titleInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(1)', el => el.innerText);
     let sizeInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(2)', el => el.innerText); */
 	browser.close();
-    return { getImages, getVideo, }
+    return { thumb, LinkAudio, }
 }
 
 ytmus.get('/', async (req, res) => {
